3fd97a96c0c3c458a8ea7d26106904658b7d0d09
王超 <wc01169074@alibaba-inc.com>
2021-12-01 15:02:19 +0800

    新增 alicloud_security_group，alicloud_vswitch， 查询

--- example/main.tf
+++ example/main.tf
@@ -8,26 +8,42 @@ data "alicloud_zones" "default" {
 }
 
 resource "alicloud_vpc" "default" {
+  count = length(data.alicloud_vpcs.default.ids) > 0 ? 0 : 1
   vpc_name   = "terraform_test"
   cidr_block = "172.16.0.0/16"
 }
 
 resource "alicloud_vswitch" "default" {
+  count = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
   vswitch_name = "terraform_test"
   cidr_block   = "172.16.0.0/16"
-  vpc_id       = alicloud_vpc.default.id
+  vpc_id       = length(data.alicloud_vpcs.default.ids) > 0 ? "${data.alicloud_vpcs.default.ids.0}" : alicloud_vpc.default.0.id
   zone_id      = data.alicloud_zones.default.zones.0.id
 }
 
 resource "alicloud_security_group" "default" {
+  count = length(data.alicloud_security_groups.default.ids) > 0 ? 0 : 1
   name   = "terraform_test"
-  vpc_id = alicloud_vpc.default.id
+  vpc_id = length(data.alicloud_vpcs.default.ids) > 0 ? "${data.alicloud_vpcs.default.ids.0}" : alicloud_vpc.default.0.id
+}
+
+data "alicloud_vpcs" "default" {
+  is_default = true
+}
+
+data "alicloud_security_groups" "default" {
+  vpc_id = length(data.alicloud_vpcs.default.ids) > 0 ? "${data.alicloud_vpcs.default.ids.0}" : alicloud_vpc.default.0.id
+}
+
+data "alicloud_vswitches" "default" {
+  zone_id = data.alicloud_zones.default.zones[0].id
+  vpc_id =  length(data.alicloud_vpcs.default.ids) > 0 ? "${data.alicloud_vpcs.default.ids.0}" : alicloud_vpc.default.0.id
 }
 
 module "example" {
   source             = "../"
   name               = var.name
-  security_group_ids = [alicloud_security_group.default.id]
-  vswitch_id         = alicloud_vswitch.default.id
+  security_group_ids = length(data.alicloud_security_groups.default.ids) > 0 ? [data.alicloud_security_groups.default.ids.0] : [alicloud_security_group.default.0.id]
+  vswitch_id         = length(data.alicloud_vswitches.default.ids) > 0 ? "${data.alicloud_vswitches.default.ids.0}" : alicloud_vswitch.default.0.id
   availability_zone  = data.alicloud_zones.default.zones.0.id
 }
