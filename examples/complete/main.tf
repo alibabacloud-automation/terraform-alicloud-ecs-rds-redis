44b39c7fc5d1b4b60b9522e01a5ed147088fcdb6
shanye <shanye.xzq@alibaba-inc.com>
2025-10-15 17:56:28 +0800

    fix: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,17 +1,29 @@
+provider "alicloud" {
+  region = "ap-southeast-5"
+}
+
 data "alicloud_resource_manager_resource_groups" "default" {
 }
 
-data "alicloud_zones" "default" {
-  available_resource_creation = "KVStore"
+data "alicloud_db_zones" "default" {
+  engine         = "MySQL"
+  engine_version = "5.6"
+}
+
+locals {
+  zone_id = data.alicloud_db_zones.default.zones[0].id
 }
 
 data "alicloud_images" "default" {
-  name_regex = "^centos_6"
+  most_recent   = true
+  instance_type = data.alicloud_instance_types.default.instance_types[0].id
 }
 
 data "alicloud_instance_types" "default" {
-  availability_zone    = data.alicloud_zones.default.zones[0].id
-  instance_type_family = "ecs.c6"
+  availability_zone    = local.zone_id
+  cpu_core_count       = 2
+  memory_size          = 8
+  instance_type_family = "ecs.g9i"
 }
 
 data "alicloud_db_instance_classes" "default" {
@@ -20,7 +32,7 @@ data "alicloud_db_instance_classes" "default" {
 }
 
 data "alicloud_kvstore_instance_classes" "default" {
-  zone_id        = data.alicloud_zones.default.zones[0].id
+  zone_id        = local.zone_id
   engine         = "Redis"
   engine_version = var.redis_engine_version
 }
@@ -34,7 +46,7 @@ module "vpc" {
   vpc_cidr           = "172.16.0.0/16"
   vswitch_name       = var.name
   vswitch_cidrs      = ["172.16.0.0/21"]
-  availability_zones = [data.alicloud_zones.default.zones[0].id]
+  availability_zones = [local.zone_id]
 }
 
 module "security_group" {
@@ -49,18 +61,18 @@ module "example" {
   name               = var.name
   security_group_ids = [module.security_group.this_security_group_id]
   vswitch_id         = module.vpc.this_vswitch_ids[0]
-  availability_zone  = data.alicloud_zones.default.zones[0].id
+  availability_zone  = local.zone_id
 
   #alicloud_instance
   instance_type              = data.alicloud_instance_types.default.instance_types[0].id
-  system_disk_category       = "cloud_efficiency"
+  system_disk_category       = "cloud_essd"
   system_disk_name           = var.system_disk_name
   system_disk_description    = var.system_disk_description
   image_id                   = data.alicloud_images.default.images[0].id
   internet_max_bandwidth_out = var.internet_max_bandwidth_out
   ecs_size                   = 1200
   data_disks_name            = "data_disks_name"
-  category                   = "cloud_efficiency"
+  category                   = "cloud_essd"
   description                = "tf-ecs-rds-redis-description"
   encrypted                  = true
 
